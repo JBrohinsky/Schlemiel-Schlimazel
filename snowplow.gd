ea34e1cc2c7f2bada6e59ae52c2daf6373be635b
JBrohinsky <10footfall@gmail.com>
2024-01-27 12:38:02 -0600

    Added Snowplow scene

---
+++ snowplow.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_just_pressed("backspace"):
+		#linear_velocity.x = -50000 * delta
+		add_constant_force(Vector2(-500000 * delta,0))
+		
+#
