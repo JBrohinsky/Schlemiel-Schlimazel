7f0eacc76ca26eb1e3d7b9a549723607f79d39a1
JBrohinsky <10footfall@gmail.com>
2024-01-28 01:00:22 -0600

    Timing changes
    Set timer to 70 seconds which is right on the edge of my skill level. Right now the progress bar counts up, will want to swap that when I can finally remember how to math again.
    
    Changed name of game too.

--- scenes/levels/plow_scene.gd
+++ scenes/levels/plow_scene.gd
@@ -10,7 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pb.value =  timer.time_left
+	pb.value =  (1 - timer.time_left / timer.wait_time) * 100
 
 
 func _on_timer_timeout():
