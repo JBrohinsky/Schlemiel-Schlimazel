3aa09b7a66a973331397aacd05210b6b9b72f590
JBrohinsky <10footfall@gmail.com>
2024-01-28 00:45:12 -0600

    Added progress bar for plow
    Added time limit for when you get smacked by the plow -- currently 100 seconds.

---
+++ scenes/levels/plow_scene.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var timer = $Timer
+@onready var pb = $SnowplowComing
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	timer.wait_time = pb.value # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pb.value =  timer.time_left
+
+
+func _on_timer_timeout():
+	$SnowPlow/Snowplow.launch_snowplow() # Replace with function body.
--- scenes/snowplow/snowplow.gd
+++ scenes/snowplow/snowplow.gd
@@ -5,13 +5,16 @@ extends RigidBody2D
 func _ready():
 	pass # Replace with function body.
 
+func launch_snowplow():
+	add_constant_force(Vector2(-100000, 0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if Input.is_action_just_pressed("backspace"):
+	pass
+	#if Input.is_action_just_pressed("backspace"):
 		#linear_velocity.x = -50000 * delta
 		#velocity.x = -500000 * delta
 		#move_and_slide()
-		add_constant_force(Vector2(-7000000 * delta, 0))
+		#add_constant_force(Vector2(-7000000 * delta, 0))
 		
 #
